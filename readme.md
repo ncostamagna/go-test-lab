cb6b99fd8025b9891c45c51e35b3bb0f662352e7
Nahue <nlcostamagna@gmail.com>
2025-04-04 16:35:44 -0300

    suite test

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Suite
+We can split **unit test** and **integration test**
+
+## Lifecycles
+Every test has its lifecycle (setup, before test, after test, cleanup). We can set code in each steps
---
+++ suites/example/examplesuite_test.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type ExampleSuite struct {
+	suite.Suite
+}
+
+func TestExampleSuite(t *testing.T) {
+	suite.Run(t, new(ExampleSuite))
+}
+
+func (suite *ExampleSuite) TestExample() {
+	suite.Equal(1, 1)
+}
+
+func (suite *ExampleSuite) TestExample2() {
+	suite.Equal(2, 2)
+}
+
+// SetupSuite is called once before the tests in the suite are run
+func (suite *ExampleSuite) SetupSuite() {
+	suite.T().Log("SetupSuite") // log in the test output
+}
+
+// TearDownSuite is called once after the tests in the suite are run
+// close database, etc.
+func (suite *ExampleSuite) TearDownSuite() {
+	suite.T().Log("TearDownSuite") // log in the test output
+}
+
+// SetupTest is called before each test
+func (suite *ExampleSuite) SetupTest() {
+	suite.T().Log("SetupTest") // log in the test output
+}
+
+// TearDownTest is called after each test
+func (suite *ExampleSuite) TearDownTest() {
+	suite.T().Log("TearDownTest") // log in the test output
+}
+
+func (suite *ExampleSuite) BeforeTest(suiteName, testName string) {
+	suite.T().Logf("BeforeTest: %s, %s", suiteName, testName) // log in the test output
+}
+
+func (suite *ExampleSuite) AfterTest(suiteName, testName string) {
+	suite.T().Logf("AfterTest: %s, %s", suiteName, testName) // log in the test output
+}
+
+
